32b01fb2f9f967abf30d49e834820fb11098399c
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-06-12 18:06:27 +0530

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,50 @@
+//Variables
+// var firstName = 'Nabendu';
+// let lastName = "Biswas"
+// var age = 41;
+// age = 40;
+// const isDeveloper = true;
+// // isDeveloper = false;
+// console.log(firstName)
+// console.log(lastName)
+// console.log(age)
+// console.log(isDeveloper)
+
+//Difference between var and let
+// function varExp(){
+//     if(true){
+//         var num1 = 10;
+//         console.log('Inside if var ' + num1);
+//     }
+//     console.log('Outside if var ' + num1);
+// }
+
+// varExp();
+// console.log('Outside function var ' + num1);
+
+
+// if(true){
+//     let num2 = 20;
+//     console.log('Inside if let ' + num2);
+// }
+// console.log('Outside if let ' + num2);
+
+//DataTypes
+const firstName = 'Nabendu';
+const age = 41;
+const rating = 5.5;
+const isDeveloper = true;
+const x = null;
+let y;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isDeveloper);
+console.log(typeof x);
+console.log(typeof y);
+
+if(typeof x == 'object'){
+    console.log("This is null")
+}
+
