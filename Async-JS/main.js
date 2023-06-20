a22308c93f3cc8492d6a30e3b9b8fc610dac7bd1
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-06-20 18:13:11 +0530

    Add files via upload

---
+++ Async-JS/main.js
@@ -0,0 +1,47 @@
+//Async JS - The Problem
+const posts = [
+    {title: 'JS Basics', summary: 'This post contains the basics of JS'},
+    {title: 'DOM Basics', summary: 'This post contains the basics of DOM'}
+]
+
+function getPosts(){
+    setTimeout(() => {
+        let output = '';
+        posts.forEach(post => output += `<h3>${post.title}</h3><p>${post.summary}</p>`);
+        document.body.innerHTML = output;
+    }, 1000)
+}
+
+//Async JS - Promises - then..catch
+function createPost(post){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            const num = 3;
+            num > 4 ? resolve() : reject('Error: Something is wrong');
+        }, 2000)
+    })
+}
+
+createPost({title: 'Async Basics', summary: 'This post contains the basics of Async'})
+            .then(getPosts)
+            .catch(err => document.body.innerHTML = `${err}`)
+
+// function createPost(post){
+//     setTimeout(() => {
+//         posts.push(post)
+//     }, 2000)
+// }
+
+// getPosts();
+// createPost({title: 'Async Basics', summary: 'This post contains the basics of Async'});
+
+//Async JS - Callbacks
+// function createPost(post, callback){
+//     setTimeout(() => {
+//         posts.push(post)
+//         callback()
+//     }, 2000)
+// }
+
+// createPost({title: 'Async Basics', summary: 'This post contains the basics of Async'}, getPosts)
