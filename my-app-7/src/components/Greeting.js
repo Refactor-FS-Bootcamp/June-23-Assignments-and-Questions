dec9c08964b51693991168227d02a8c9f2618e38
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-07-05 18:23:45 +0530

    Add files via upload

---
+++ my-app-7/src/components/Greeting.js
@@ -0,0 +1,12 @@
+const Greeting = (props) => {
+    const { name, type, children } = props;
+    
+    return (
+        <>
+            <h1>Greeting from {name} which is {type}.</h1>
+            <p>{children}</p>
+        </>
+    )
+}
+
+export default Greeting
